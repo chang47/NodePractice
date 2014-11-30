1b85bf4bccbbff16dec67d1459842b6b5e809c30
chang47 <coralbue@gmail.com>
2014-11-29 19:16:05 -0800

    requires redis to continue, currently paused

--- queue/simple-job-queue.js
+++ queue/simple-job-queue.js
@@ -1,10 +1,25 @@
-var kue = require('kue')
-  , jobs = kue.createQueue();
+var kue = require('kue'),
+jobs = kue.createQueue();
 
-  var job = jobs.create('email', {
-    title: 'welcome email for tj'
-  , to: 'tj@learnboost.com'
-  , template: 'welcome-email'
-}).save( function(err){
-   if( !err ) console.log( job.id );
-});
+kue.app.listen(3000);
+var sequence = 0;
+setInterval(
+	function() {
+		sequence += 1;
+		(function(sequence) {
+			var job = jobs.create('email', {
+			title: 'Hello #' + sequence,
+			to: 'coralbue@gmail.com',
+			body: "hi"
+		}).save();
+		
+
+		job.on('complete', function() {
+			console.log('job ' + sequence + ' completed!')
+		});
+
+		job.on("failed", function() {
+			console.log(sequence + ' failed')
+		});
+	})(sequence);
+}, 1000 );
